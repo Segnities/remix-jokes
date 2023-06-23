2101d2bb7d3bcffe4be47fcb4fa01b2bebe4daf8
Gadoro Zasandalio <70950389+Segnities@users.noreply.github.com>
2023-06-23 09:36:36 +0300

    Change regular forms to remix Form component

--- app/routes/login.tsx
+++ app/routes/login.tsx
@@ -5,6 +5,7 @@ import type {
 } from "@remix-run/node";
 
 import {
+    Form,
     Link,
     useActionData,
     useSearchParams
@@ -145,7 +146,7 @@ export default function Login() {
         <div className="container">
             <div className="content" data-light="">
                 <h1>Login</h1>
-                <form method="post">
+                <Form method="post">
                     <input
                         type="hidden"
                         name="redirectTo"
@@ -216,7 +217,7 @@ export default function Login() {
                     <button type="submit" className="button">
                         Submit
                     </button>
-                </form>
+                </Form>
             </div>
             <div className="links">
                 <ul>
